42f4cdaefb936cf2534d8a0a7cbf2960f166459f
Ilosariph <71074737+Ilosariph@users.noreply.github.com>
2024-10-23 23:28:05 +0200

    Add tightarc

---
+++ keyboards/ilosariph/tightarc/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xCD, 0xD5, 0x33, 0x13, 0xDE, 0xDC, 0x1A, 0x12}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 5 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 5 }
+
+#define WS2812_DI_PIN GP8
---
+++ keyboards/ilosariph/tightarc/tightarc.c
@@ -0,0 +1,1 @@
+#include "tightarc.h"
